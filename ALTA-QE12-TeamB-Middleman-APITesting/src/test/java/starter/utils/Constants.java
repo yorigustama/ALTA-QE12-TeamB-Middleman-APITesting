65467096f5035dadd372fea96dc4737155f6e6db
yorigustama <yfiandika@gmail.com>
2023-10-27 19:43:50 +0700

    Fist Commit

---
+++ ALTA-QE12-TeamB-Middleman-APITesting/src/test/java/starter/utils/Constants.java
@@ -0,0 +1,17 @@
+package starter.utils;
+
+public class Constants {
+
+    public final static String BASE_URL = "https://dummyjson.com";
+
+    public static String DIR = System.getProperty("user.dir");
+    public static String JSON = DIR+"/src/test/resources/JSON";
+    public static String REQ_BODY_POST_CREATE_NEW_USER = JSON+ "/JSON/ReqBody";
+    public static String REQ_BODY = JSON + "/JSON/ReqBody/";
+    public static String JSON_SCHEMA = JSON+"/JSONSchema/";
+
+
+
+
+
+}
