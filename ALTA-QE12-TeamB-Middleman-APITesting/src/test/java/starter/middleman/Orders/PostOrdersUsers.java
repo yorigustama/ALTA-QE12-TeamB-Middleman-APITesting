022b17d2fadd2866febe4b61662cad9b7985e68b
yorigustama <yfiandika@gmail.com>
2023-10-27 19:48:05 +0700

    First Commit

--- ALTA-QE12-TeamB-Middleman-APITesting/src/test/java/starter/middleman/Orders/PostOrdersUsers.java
+++ ALTA-QE12-TeamB-Middleman-APITesting/src/test/java/starter/middleman/Orders/PostOrdersUsers.java
@@ -1,4 +1,7 @@
 package starter.middleman.Orders;
 
+import starter.utils.Constants;
+
 public class PostOrdersUsers {
+    public static String POST_ORDERS_USERS = Constants.BASE_URL+"orders/users";
 }
--- ALTA-QE12-TeamB-Middleman-APITesting/src/test/java/starter/utils/Constants.java
+++ ALTA-QE12-TeamB-Middleman-APITesting/src/test/java/starter/utils/Constants.java
@@ -2,7 +2,7 @@ package starter.utils;
 
 public class Constants {
 
-    public final static String BASE_URL = "https://dummyjson.com";
+    public final static String BASE_URL = "https://middleman.altapro.online/";
 
     public static String DIR = System.getProperty("user.dir");
     public static String JSON = DIR+"/src/test/resources/JSON";
